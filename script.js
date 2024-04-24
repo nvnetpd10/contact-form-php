6693d43cd86ed1ee5e7455bb02a36c842c810d97
nvnetpd10 <navneettiwarinew270103@gmail.com>
2024-04-24 05:45:46 +0530

    added

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("form");
+const statusTxt = form.querySelector(".button-area span");
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  statusTxt.style.color = "#0D6EFD";
+  statusTxt.style.display = "block";
+  statusTxt.innerText = "Sending your message...";
+  form.classList.add("disabled");
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "message.php", true);
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let response = xhr.responseText;
+      if (response.includes("required") || response.includes("valid") || response.includes("failed")) {
+        statusTxt.style.color = "red";
+      } else {
+        form.reset();
+        setTimeout(() => {
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+      statusTxt.innerText = response;
+      form.classList.remove("disabled");
+    }
+  };
+  let formData = new FormData(form);
+  xhr.send(formData);
+};
